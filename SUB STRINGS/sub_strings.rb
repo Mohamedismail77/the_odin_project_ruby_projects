7598ccc61be1e53dec62e94f03f0ba2a4f173760
Mohamed Ismail <mohamedismail77@yahoo.com>
2021-10-26 15:26:12 +0200

    enhance substring solution

--- SUB STRINGS/sub_strings.rb
+++ SUB STRINGS/sub_strings.rb
@@ -1,10 +1,10 @@
-def substrings(input,dictionary)
-    data = input.scan(/\w+/)
-    output = data.flatten.to_h{|word| [word.downcase.strip(),dictionary.count(word.downcase.strip)]}
-    output.select{|key,value| value > 0}
+def substrings(input,dictionary)   
+    data = dictionary.map{|word| input.downcase.scan(word)}.flatten
+    data.to_h{|word| [word,data.count(word)]}
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
  ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
 
